a11ea927afa4d333f0ef8073fa9b59d225efef99
YoungWook Choi <duddnr3618@gmail.com>
2024-10-29 18:56:28 +0900

    매매시뮬 2

---
+++ src/main/java/com/example/javainit/trading/controller/TradingController.java
@@ -0,0 +1,16 @@
+package com.example.javainit.trading.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/trading")
+public class TradingController {
+
+    @GetMapping("simulation")
+    public String simulation() {
+        return "trading/simulation";
+    }
+
+}
