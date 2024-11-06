a0208414bf33740c7bd13bcf05f42b17e30e316b
YoungWook Choi <duddnr3618@gmail.com>
2024-11-06 10:06:34 +0900

    계산식5

--- src/main/resources/static/trading/js/simulation.js
+++ src/main/resources/static/trading/js/simulation.js
@@ -1,12 +1,18 @@
 function handleCustomInputs() {
-    // 모든 custom-select와 custom-input 요소를 쌍으로 처리
     const selects = document.querySelectorAll('.custom-select');
     const inputs = document.querySelectorAll('.custom-input');
 
     selects.forEach((select, index) => {
-        // "custInputOption"이 선택된 경우 해당 input을 표시하고, 아니면 숨김
         inputs[index].style.display = (select.value === "custInputOption") ? "inline" : "none";
+        // 선택한 옵션을 직접 입력한 값으로 설정
+        if (select.value !== "custInputOption") {
+            inputs[index].value = ''; // 입력창을 초기화
+        }
     });
+}
+
+function updateCustomInput(fieldName, value) {
+    document.getElementById(fieldName).value = value;
 }
 
 function InputData () {
